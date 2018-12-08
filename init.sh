f08bd50ceda0c9f04391ef8d3b6deae83cf698a3
Anton Österberg <anton.osterberg@gmail.com>
2018-12-08 18:53:31 +0100

    Improvements to init.sh - do not copy init.sh at project creation and read github username from git config.

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 mkdir ../$1 || exit
-cp --parents -rvn $(git ls-files) ../$1 &&
+cp --parents -rvn $(git ls-files|grep -v $0) ../$1 &&
 cd ../$1 &&
 git init &&
-git remote add origin git@github.com:mantono/$1.git &&
+user=$(git config --global credential.git@github.com.username)
+git remote add origin git@github.com:$user/$1.git &&
 sed -i -e "s/{{project_name}}/$1/g" settings.gradle

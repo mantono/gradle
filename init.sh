dcc42b2399f41559acdc950c49fe0443a4e17d70
Anton Österberg <anton.osterberg@gmail.com>
2019-02-25 00:19:39 +0100

    Change more templates

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ cd ../$1 &&
 git init &&
 user=$(git config --global credential.git@github.com.username)
 git remote add origin git@github.com:$user/$1.git &&
-sed -i -e "s/{{project_name}}/$1/g" settings.gradle
+sed -i -e "s/{{project_name}}/$1/g" *.gradle.kts README.md
+sed -i -e "s/{{project_description}}/$2/g" *.gradle.kts README.md
